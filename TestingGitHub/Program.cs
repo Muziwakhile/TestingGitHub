8ca023163da51e734ab545fb95193cee9da9fcf7
Mzwakhilet@gmail.com <Mzwakhilet@gmail.com>
2019-01-02 16:57:17 +0200

    Added Console read

--- TestingGitHub/Program.cs
+++ TestingGitHub/Program.cs
@@ -11,6 +11,7 @@ namespace TestingGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from GitHub");
+            Console.Read();
         }
     }
 }
